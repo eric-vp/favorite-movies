71788e0b765a6d8fed95df52ee80f32be825c980
lucasvieirap <ericvpalavro@gmail.com>
2024-01-19 19:21:38 -0300

    removendo codigos comentados

--- modules/fetchPopulares.js
+++ modules/fetchPopulares.js
@@ -2,14 +2,6 @@ import { apiMovie } from "./apiMovie.js";
 import { desativaCheckbox } from "./desativaCheckbox.js";
 import { options } from "./options.js";
 
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         accept: 'application/json',
-//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OGMyMTI4MDlmM2M0YTQ0M2NhMGQzM2Q0ZTA0OWU5ZCIsInN1YiI6IjY0OWRmMTFjYzA3MmEyMDE0ZmFmMjJjMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j5aw_-wVNkc7towwc7N8uUHlw-GA2HdoPlmwDAhDnmM'
-//     }
-// }
-
 export function fetchPopulares() {
     const urlPopular = 'https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1';
     fetch(urlPopular, options)
