480e896d0002f4da84c0a3bef2fc9f95bab57762
lucasvieirap <ericvpalavro@gmail.com>
2024-01-19 16:45:51 -0300

    atualizando segurança

--- modules/options.js
+++ modules/options.js
@@ -1,7 +1,9 @@
+import { apiKey } from "./apiKey";
+
 export const options = {
     method: 'GET',
     headers: {       
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OGMyMTI4MDlmM2M0YTQ0M2NhMGQzM2Q0ZTA0OWU5ZCIsInN1YiI6IjY0OWRmMTFjYzA3MmEyMDE0ZmFmMjJjMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j5aw_-wVNkc7towwc7N8uUHlw-GA2HdoPlmwDAhDnmM'
+        Authorization: apiKey
     }
 }
